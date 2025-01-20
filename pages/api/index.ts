0c28a2e75236c775430fef1a10e31b73c1f2c3cc
arash16 <arash.shakery@gmail.com>
2025-01-21 01:56:16 +0330

    cors all

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -5,16 +5,27 @@ export const config = {
 }
 
 export default async function handler(req: NextRequest) {
-  const url = new URL(req.url)
-  url.host = 'api.openai.com'
-  url.pathname = url.pathname.replace(/^\/api/, '')
-  return fetch(
-    url.toString(),
+  const url = new URL(req.url);
+  url.host = 'api.openai.com';
+
+  const headers = new Headers();
+  const auth = req.headers.get('Authorization');
+  if (auth) headers.set('Authorization', auth);
+
+  const response = await fetch(url, {
+    method: req.method,
+    headers: headers,
+    body: req.method !== 'GET' ? req.body : null,
+  });
+
+  return new Response(
+    response.body,
     {
-      method: req.method,
-      headers: req.headers,
-      body: req.body,
-      signal: req.signal,
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
     }
-  )
+  );
 }
